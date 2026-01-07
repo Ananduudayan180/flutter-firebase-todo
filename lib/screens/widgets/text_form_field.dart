057e026031591ab7d9395f46aaa0946f832ddeb4
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-07 20:48:37 +0530

    feat: add obscureText support to form field

--- lib/screens/widgets/text_form_field.dart
+++ lib/screens/widgets/text_form_field.dart
@@ -4,18 +4,21 @@ class AppTextFormField extends StatelessWidget {
   final String hintText;
   final String errorMsg;
   final TextEditingController controller;
+  final bool obscureText;
 
   const AppTextFormField({
     super.key,
     required this.hintText,
     required this.errorMsg,
     required this.controller,
+    this.obscureText = false,
   });
 
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
     return TextFormField(
+      obscureText: obscureText,
       controller: controller,
       style: themeData.textTheme.bodyLarge,
       decoration: InputDecoration(
