5a9f93cb43de1258cf7c012d3eff6e72bf175970
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-07 21:01:45 +0530

    refactor: reeeplace sign up fields with reusable widgets

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task_flow/screens/widgets/custom_button.dart';
+import 'package:task_flow/screens/widgets/text_form_field.dart';
 
 class SignUpPage extends StatelessWidget {
   SignUpPage({super.key});
@@ -21,112 +23,55 @@ class SignUpPage extends StatelessWidget {
           },
         ),
       ),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Form(
-          key: _signUpKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Create your Account',
-                style: themeData.textTheme.titleMedium,
-              ),
-              SizedBox(height: 10),
-              //Name TextFormField
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TextFormField(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Form(
+            key: _signUpKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Create your Account',
+                  style: themeData.textTheme.titleMedium,
+                ),
+                SizedBox(height: 10),
+                //Name TextFormField
+                AppTextFormField(
+                  hintText: 'Name',
+                  errorMsg: 'Name is required',
                   controller: _nameController,
-                  style: themeData.textTheme.bodyLarge,
-                  decoration: InputDecoration(
-                    hintText: 'Name',
-                    hintStyle: themeData.textTheme.bodyMedium,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Name is required';
-                    } else {
-                      return null;
-                    }
-                  },
                 ),
-              ),
-              SizedBox(height: 10),
-              //Email TextFormField
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TextFormField(
+                SizedBox(height: 10),
+                //Email TextFormField
+                AppTextFormField(
+                  hintText: 'Email',
+                  errorMsg: 'Email is required',
                   controller: _emailController,
-                  style: themeData.textTheme.bodyLarge,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: themeData.textTheme.bodyMedium,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email is required';
-                    } else {
-                      return null;
-                    }
-                  },
                 ),
-              ),
-              SizedBox(height: 10),
-              //Pass TextFormField
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TextFormField(
+                SizedBox(height: 10),
+                //Pass TextFormField
+                AppTextFormField(
+                  hintText: 'Password',
+                  errorMsg: 'Password is required',
                   controller: _passwordController,
                   obscureText: true,
-                  style: themeData.textTheme.bodyLarge,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: themeData.textTheme.bodyMedium,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password is required';
-                    } else {
-                      return null;
+                ),
+                SizedBox(height: 10),
+                //Login Button
+                CustomButton(
+                  buttonName: 'Sign up',
+                  onPressed: () {
+                    if (_signUpKey.currentState?.validate() ?? false) {
+                      //Handle sign up action
                     }
                   },
                 ),
-              ),
-              SizedBox(height: 10),
-              //Login Button
-              InkWell(
-                onTap: () {
-                  // Sign up action
-                  if (_signUpKey.currentState?.validate() ?? false) {}
-                },
-                child: Container(
-                  height: 48,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    color: Colors.teal,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Sign up',
-                      style: themeData.textTheme.bodyLarge,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
