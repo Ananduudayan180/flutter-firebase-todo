d7e7e384026027669431b915fba5171f5446af2e
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-08 12:30:26 +0530

    feat: wrap content with SingleChildScrollView and ConstrainedBox

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -28,49 +28,57 @@ class SignUpPage extends StatelessWidget {
         child: SizedBox(
           height: double.infinity,
           width: double.infinity,
-          child: Form(
-            key: _signUpKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Create your Account',
-                  style: themeData.textTheme.titleMedium,
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight:
+                    MediaQuery.of(context).size.height - kToolbarHeight - 32,
+              ),
+              child: Form(
+                key: _signUpKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Create your Account',
+                      style: themeData.textTheme.titleMedium,
+                    ),
+                    SizedBox(height: 10),
+                    //Name TextFormField
+                    AppTextFormField(
+                      hintText: 'Name',
+                      errorMsg: 'Name is required',
+                      controller: _nameController,
+                    ),
+                    SizedBox(height: 10),
+                    //Email TextFormField
+                    AppTextFormField(
+                      hintText: 'Email',
+                      errorMsg: 'Email is required',
+                      controller: _emailController,
+                    ),
+                    SizedBox(height: 10),
+                    //Pass TextFormField
+                    AppTextFormField(
+                      hintText: 'Password',
+                      errorMsg: 'Password is required',
+                      controller: _passwordController,
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 10),
+                    //Login Button
+                    CustomButton(
+                      buttonName: 'Sign up',
+                      onPressed: () {
+                        if (_signUpKey.currentState?.validate() ?? false) {
+                          //Handle sign up action
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
-                //Name TextFormField
-                AppTextFormField(
-                  hintText: 'Name',
-                  errorMsg: 'Name is required',
-                  controller: _nameController,
-                ),
-                SizedBox(height: 10),
-                //Email TextFormField
-                AppTextFormField(
-                  hintText: 'Email',
-                  errorMsg: 'Email is required',
-                  controller: _emailController,
-                ),
-                SizedBox(height: 10),
-                //Pass TextFormField
-                AppTextFormField(
-                  hintText: 'Password',
-                  errorMsg: 'Password is required',
-                  controller: _passwordController,
-                  obscureText: true,
-                ),
-                SizedBox(height: 10),
-                //Login Button
-                CustomButton(
-                  buttonName: 'Sign up',
-                  onPressed: () {
-                    if (_signUpKey.currentState?.validate() ?? false) {
-                      //Handle sign up action
-                    }
-                  },
-                ),
-              ],
+              ),
             ),
           ),
         ),
