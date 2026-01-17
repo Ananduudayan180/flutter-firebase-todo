caf3701fc2315a9db7c1f3dee46853b072256b0a
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-17 11:55:49 +0530

    feat: add addUserTask method in TaskService

---
+++ lib/services/task_service.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:task_flow/models/task_model.dart';
+
+class TaskService {
+  final CollectionReference _taskCollection = FirebaseFirestore.instance
+      .collection('tasks');
+
+  Future<void> addUserTask(TaskModel task) async {
+    await _taskCollection.doc(task.id).set(task.toJson());
+  }
+}
